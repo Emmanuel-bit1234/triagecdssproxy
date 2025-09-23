a5c8058801e0f6245821ac3fce197c6b1705d329
Emmanuel Mbuya <emmechcool@gmail.com>
2025-09-23 07:56:39 +0200

    updated deploy

--- src/index.ts
+++ src/index.ts
@@ -40,7 +40,7 @@ app.post("/predict", authMiddleware, async (context) => {
     console.log(`Prediction request from user: ${user.email}`);
 
     const body = await context.req.json();
-    const result = await fetch("https://38d225b56cc4.ngrok-free.app/model1", {
+    const result = await fetch("https://c7f4c7438149.ngrok-free.app/model1", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
